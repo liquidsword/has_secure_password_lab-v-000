aaf25ca84a9911aecbbaf8df2f013bd2e2bd6908
Auden Robertson <audenrobertson@gmail.com>
2018-06-10 20:21:22 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :current_user
+
   def hello
     redirect_to controller: 'sessions', action: 'new' unless session[:name]
   end
