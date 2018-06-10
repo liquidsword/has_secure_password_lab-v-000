ac4a9e643b74cc836f1bf2bcdc6853ce9f5952d2
Auden Robertson <audenrobertson@gmail.com>
2018-06-10 20:01:04 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < Application:Controller
     @user = User.create(user_params)
     return redirect_to controller: 'users', action: 'new' unless @user.save
     session[:user_id]= @user.id
+    redirect_to controller: 'welcome', action: 'home'
 
   end
 
